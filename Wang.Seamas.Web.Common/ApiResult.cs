6c71c053e61e2c2675d9a98bae8bd27f5184b4ff
Seamas Wang <seamas.wang@hotmail.com>
2026-02-01 20:41:03 +0800

    添加时间戳

--- Wang.Seamas.Web.Common/ApiResult.cs
+++ Wang.Seamas.Web.Common/ApiResult.cs
@@ -5,6 +5,9 @@ namespace Wang.Seamas.Web.Common
         public bool Success { get; set; }
         public string? Message { get; set; }
 
+        public long Timestamp => DateTime.UtcNow.Ticks;
+
+
         public static ApiResult Ok() => new() { Success = true };
         public static ApiResult Fail(string message) => new() { Success = false, Message = message };
     }
@@ -14,6 +17,8 @@ namespace Wang.Seamas.Web.Common
         public bool Success { get; set; }
         public string? Message { get; set; }
         public T? Data { get; set; }
+        
+        public long Timestamp => DateTime.UtcNow.Ticks;
 
         public static ApiResult<T> Ok(T data) => new() { Success = true, Data = data };
         public static ApiResult<T> Fail(string message) => new() { Success = false, Message = message };
